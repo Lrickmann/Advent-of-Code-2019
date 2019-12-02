951b01e49f81689b60df94d8a93469928b8b3385
Lrickmann <lennart.rickmann@gmail.com>
2019-12-02 15:44:04 +0100

    shortend incode

--- day02/intcode.py
+++ day02/intcode.py
@@ -3,15 +3,12 @@ import numpy as np
 
 class Intcode(object):
 
+    """extandable compute function"""
     def __init__(self, input_name, func_mode=None, func=None):
         self.array_orig = np.loadtxt(input_name, delimiter=",", dtype=int)
         self.compute_func_mode = func_mode
         self.compute_func = func
 
-    """
-        Berechnet den nächsten Schritt, abhängig vom mode.
-        Kann eine im Konstruktor definierte Formel übernehmen und anwenden
-    """
     def compute(self, arr, mode, first, second, target):
         if mode == 1:
             arr[target] = arr[first] + arr[second]
@@ -28,34 +25,7 @@ class Intcode(object):
             print(mode)
             raise Exception("falscher mode")
 
-    def get_noun_and_verb(self):
-        done = False
-        counter = 4
-
-        for noun in range(100):
-            for verb in range(100):
-                boolean = True
-                i = 0
-                array = self.array_orig.copy()
-                array[1] = noun
-                array[2] = verb
-                while boolean:
-                    if array[i] == 99:
-                        i += 1
-                        boolean = False
-                    else:
-                        array, boolean = self.compute(array, array[i], array[i+1], array[i+2], array[i+3])
-                        i += counter
-                if array[0] == 19690720:
-                    print('Your noun has to be: ', noun)
-                    print('Your verb has to be: ', verb)
-                    print('100 * noun + verb = ', 100 * noun + verb)
-                    done = True
-                    break
-            if done:
-                break
-
-    def get_first(self, noun, verb):
+    def get_first(self, noun, verb, printbool=True):
         boolean = True
         i = 0
         array = self.array_orig.copy()
@@ -64,10 +34,26 @@ class Intcode(object):
         while boolean:
             array, boolean = self.compute(array, array[i], array[i + 1], array[i + 2], array[i + 3])
             i += 4
-        print("Your value at position 0 is ", array[0])
+        if printbool:
+            print("Your value at position 0 is ", array[0])
+        else:
+            return array[0]
+
+    def get_noun_and_verb(self):
+        done = False
+        for noun in range(100):
+            for verb in range(100):
+                if self.get_first(noun, verb, printbool=False) == 19690720:
+                    print('Your noun has to be: ', noun)
+                    print('Your verb has to be: ', verb)
+                    print('100 * noun + verb = ', 100 * noun + verb)
+                    done = True
+                    break
+            if done:
+                break
 
 
 if __name__ == '__main__':
     test = Intcode('../inputs/input02')
-    test.get_noun_and_verb()
     test.get_first(12, 2)
+    test.get_noun_and_verb()
